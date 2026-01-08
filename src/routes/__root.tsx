4002718d6cac6cfc61155c994758aff4c16f91d7
Zero <166997982@qq.com>
2026-01-08 14:27:38 +0800

    chore: 暂时禁用 TanStack Router DevTools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,9 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { RootLayout } from "@/components/layout/RootLayout";
+
+// 是否显示 TanStack Router DevTools（开发时按需启用）
+// const SHOW_ROUTER_DEVTOOLS = false;
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,9 +13,11 @@ function RootComponent() {
   return (
     <RootLayout>
       <Outlet />
-      {import.meta.env.DEV && (
+      {/* 暂时禁用 DevTools，需要时取消注释即可
+      {import.meta.env.DEV && SHOW_ROUTER_DEVTOOLS && (
         <TanStackRouterDevtools position="bottom-right" />
       )}
+      */}
     </RootLayout>
   );
 }
